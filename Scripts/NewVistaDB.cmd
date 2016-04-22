b00596dbcf3a46c836b1bfcd394428fa28b6c064
Mike Baker <michael.baker@apexdatasolutions.net>
2016-04-22 00:15:18 +0000

    Added Scripts/NewVistaDB.cmd

---
+++ Scripts/NewVistaDB.cmd
@@ -0,0 +1,5 @@
+C:\InterSystems\Cache\bin\ccontrol stop CACHE
+XCopy /Y D:\CACHE.DAT C:\CacheDB\bay\
+XCopy /Y D:\CACHE.DAT C:\CacheDB\tam\
+XCopy /Y D:\CACHE.DAT C:\CacheDB\wpb\
+C:\InterSystems\Cache\bin\ccontrol start CACHE
